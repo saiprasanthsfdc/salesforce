911bc9a6d7b656f042ccae09f5299837005c5b60
Puvvvada <S.Puvvvada@shell.com>
2020-02-05 16:37:44 +0530

    update for this test

--- force-app/main/default/lwc/testingGit/testingGit.js
+++ force-app/main/default/lwc/testingGit/testingGit.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class TestingGit extends LightningElement {}
+export default class TestingGit extends LightningElement {
+
+    connectedCallback()
+    {
+        console.log('This is conencted callback is loading now raja');
+    }
+}
